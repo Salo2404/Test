15c773591e9cd246c48fef61f3dd711538fe85c7
Artur <122271936+Salo2404@users.noreply.github.com>
2024-04-19 16:40:06 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@ let questions = [
         correctAnswer: "Функция"
     },
     {
-        question: "Какую тему по математике вы сейчас проходите",
-        options: ["Функция", "Дроби", "Степени", "+ и - числа"],
-        correctAnswer: "Функция"
+        question: "Решите пример: 2 + 2 * 2",
+        options: ["8", "6", "Не знаю", "Вам надо вы и решайте"],
+        correctAnswers: "6"
     },
     {
-        question: "Какую тему по математике вы сейчас проходите",
-        options: ["Функция", "Дроби", "Степени", "+ и - числа"],
-        correctAnswer: "Функция"
+        question: "Назовите формулу функции",
+        options: ["y = kx + b", "2a + 2b", "a + b", "2ab"],
+        correctAnswer: "У = kx + b"
     },
 
 ];
 
 let currentQuestion = 0; //Текущий вопрос
-let currentAnswers = 0; //Количество вопросов
+let correctAnswers = 0;//Количество правильных ответов
 
 //Функция для отображения текущего вопроса и вариантов ответов
 function displayQuestion() {
@@ -44,11 +44,32 @@ function displayQuestion() {
     optionsElement.addEventListener('click', (e) => {
         //Получаем в переменную кпопку на которую кликнули
         let target = e.target;
+        console.log(target);
         //Вызовем функцию проверки ответа на корректность и перехода к следующему вопросу (в аргумент функции передаем текст ответа):
         nextQuestion(target.textContent)
-    })
+    }, { once: true })
 
 }
 
+function nextQuestion(answer) {
+    if (answer === questions[currentQuestion].correctAnswer) {
+        correctAnswers++;
+    }
+    currentQuestion++;
 
-displayQuestion();
+    if (currentQuestion < questions.length) {
+        displayQuestion();
+    } else {
+        displayResult();
+    }
+}
+
+function displayResult() {
+    const questionElement = document.getElementById("question");
+    const optionsElement = document.getElementById("options");
+    const resultElement = document.getElementById("result");
+    questionElement.style.display = "none";
+    optionsElement.style.display = "none";
+    resultElement.textContent = `Правильных ответов: ${correctAnswers} из ${questions.length}`;
+
+}
